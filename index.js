3fae4db048861f5c2ec0a638df00fbaa9913894b
j47in-github <77193042+j47in-github@users.noreply.github.com>
2021-03-25 16:27:49 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var ctx = canvas.getContext("2d");
 
 window.addEventListener("keydown", onKeyDown);
 
+marginX = 10;
+marginY = 10;
+
+marginX1 = 10;
+marginY1 = 100;
+
 var roverImg = new Image();
 roverImg.onload = roverDraw;
 roverImg.src = "https://i.postimg.cc/YqdnnNX1/car1.png";
@@ -16,62 +22,64 @@ bg.src = "https://i.postimg.cc/bv5d35nK/racing.jpg";
 
 function roverDraw(){
     ctx.drawImage(bg, 0, 0, 800, 600);
-    ctx.drawImage(roverImg, 10, 10, 150, 90);
-    ctx.drawImage(roverImg1, 10, 100, 150, 90);
+    ctx.drawImage(roverImg, marginX, marginY, 150, 90);
+    ctx.drawImage(roverImg1, marginX1, marginY1, 150, 90);
 }
 
 function onKeyDown(e){
     keyPressed = e.keyCode;
     
     if(keyPressed == '38'){
-        up();
+        move(0, 0, -10);
     }
     if(keyPressed == '40'){
-        down();
+        move(0, 0, 10);
     }
     if(keyPressed == '37'){
-        left();
+        move(0, -10, 0);
     }
     if(keyPressed == '39'){
-        right();
+        move(0, 10, 0);
     }
 
     if(keyPressed == '87'){
-        up1();
+        move(1, 0, -10);
     }
     if(keyPressed == '83'){
-        down1();
+        move(1, 0, 10);
     }
     if(keyPressed == '65'){
-        left1();
+        move(1, -10, 0);
     }
     if(keyPressed == '68'){
-        right1();
+        move(1, 10, 0);
     }
 }
 
-function up(){
-    console.log("up c1");
-}
-function right(){
-    console.log("right c1");
-}
-function left(){
-    console.log("left c1 ");
-}
-function down(){
-    console.log("down c1");
-}
-
-function up1(){
-    console.log("up c2");
-}
-function right1(){
-    console.log("right c2");
-}
-function left1(){
-    console.log("left c2");
-}
-function down1(){
-    console.log("down c2");
+function move(carId, moveX, moveY){
+    if(carId == 0){
+        if(!(marginX < 10 && moveX < 0)){
+            if(!(marginX > 700 && moveX > 0)){
+                marginX += moveX;
+            }
+        }
+        if(!(marginY < 10 && moveY < 0)){
+            if(!(marginY > 500 && moveY > 0)){
+                marginY += moveY;
+            }
+        }
+    }
+    else if(carId == 1){
+        if(!(marginX1 < 10 && moveX < 0)){
+            if(!(marginX1 > 700 && moveX > 0)){
+                marginX1 += moveX;
+            }
+        }
+        if(!(marginY1 < 10 && moveY < 0)){
+            if(!(marginY1 > 500 && moveY > 0)){
+                marginY1 += moveY;
+            }
+        }
+    }
+    roverDraw();
 }
